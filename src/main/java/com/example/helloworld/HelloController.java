0fa273b970a032a237b5d4a71a776daf53ee9f88
Sneha <snehamane2424@gmail.com>
2026-02-25 17:04:01 +0530

    Test cicd deployment

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World from Spring Boot!";
+        return "Hello Sneha Mane from Spring Boot!";
     }
 }
